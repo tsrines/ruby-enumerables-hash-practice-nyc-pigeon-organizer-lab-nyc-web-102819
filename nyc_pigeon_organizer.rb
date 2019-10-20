b87d12a2590f9bfa58568de904991b002ecdbbc9
Timothy Rines <tsrines@gmail.com>
2019-10-20 01:07:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,18 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
-  data.each do |key, value|
-    value.each do |new_value, names|
+  data.each do |a, b|
+    b.each do |new_value, names|
       names.each do |name|
         
         if !new_hash[name]
           new_hash[name] = {}
         end
 
-        if !new_hash[name][key]
-          new_hash[name][key] = []
+        if !new_hash[name][a]
+          new_hash[name][a] = []
         end
 
-        new_hash[name][key] << new_value.to_s
+        new_hash[name][a] << new_value.to_s
 
       end
     end
