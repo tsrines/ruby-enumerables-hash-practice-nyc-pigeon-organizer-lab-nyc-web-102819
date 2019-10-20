ba761b867c11ed5610d535efe81ddf02a01c883a
Timothy Rines <tsrines@gmail.com>
2019-10-20 01:08:37 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |a, b|
+    b.each do |changed_value, names|
+      names.each do |name|
+        
+        if !new_hash[name]
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name][a]
+          new_hash[name][a] = []
+        end
+
+        new_hash[name][a] << changed_value.to_s
+
+      end
+    end
+  end
+  new_hash
 end
